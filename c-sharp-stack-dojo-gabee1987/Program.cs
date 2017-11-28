3144694164398ad6f682756f15085a8a520a5ef6
gabee1987 <gabee1987@gmail.com>
2017-11-28 10:22:30 +0100

    push() pop() working, need debug peak()

--- c-sharp-stack-dojo-gabee1987/Program.cs
+++ c-sharp-stack-dojo-gabee1987/Program.cs
@@ -10,6 +10,29 @@ namespace c_sharp_stack_dojo_gabee1987
     {
         static void Main(string[] args)
         {
+            Stack<string> testStringStack = new Stack<string>(5);
+
+            Console.WriteLine("After adding 5 element to it: ");
+            testStringStack.Push("1st Element");
+            testStringStack.Push("2nd Element");
+            testStringStack.Push("3rd Element");
+            testStringStack.Push("4th Element");
+            testStringStack.Push("5th Element");
+            Console.WriteLine(testStringStack.ToString());
+
+            Console.WriteLine();
+
+            Console.WriteLine("After removing the last 2 element:");
+            testStringStack.Pop();
+            testStringStack.Pop();
+            Console.WriteLine(testStringStack.ToString());
+
+            Console.WriteLine("Peeking the last element:");
+            string lastItem = testStringStack.Peek();
+            Console.WriteLine(lastItem);
+
+
+            Console.ReadLine();
         }
     }
 }
---
+++ c-sharp-stack-dojo-gabee1987/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_stack_dojo_gabee1987
+{
+    class Stack<T>
+    {
+        private T[] items = null;
+        private int index = 0;
+
+        public Stack(int size)
+        {
+            items = new T[size];
+        }
+
+        public void Push(T item)
+        {
+            if (index >= items.Length) { throw new StackOverflowException(); }
+            items[index] = item;
+            index++;
+        }
+
+        public T Pop()
+        {
+            if (index <= 0) { throw new InvalidOperationException(); }
+            T removedItem = items[--index];
+            items[index] = default(T);
+            return removedItem;
+
+        }
+
+        public T Peek()
+        {
+            if (index <= 0) { throw new InvalidOperationException(); }
+            return items[items.Length - 1];
+        }
+
+        public override string ToString()
+        {
+            if (index<= 0) { throw new InvalidOperationException(); }
+            string outPut = "";
+            for (int i = 0; i < items.Count(); i++)
+            {
+                outPut += items[i] + ", ";
+            }
+            return outPut;
+        }
+
+    }
+}
